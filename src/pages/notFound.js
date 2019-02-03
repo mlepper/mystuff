b06cb0bf94a576eeb110c6a70bc03f470b98a7a7
Mark Lepper <lepperj@Marks-MacBook-Pro-2018.local>
2019-02-03 06:58:30 -0500

    simplified notfound

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -3,13 +3,13 @@ import Header from "../components/header";
 
 export default props => {
   return (
-    <React.Fragment>
+    <>
       <Header />
       <main>
         <section>
           <h1>Not Found, bruh.</h1>
         </section>
       </main>
-    </React.Fragment>
+    </>
   );
 };
