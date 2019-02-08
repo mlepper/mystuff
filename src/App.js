5fbf80fcc61d995805dad28efeff74d9c99432d9
Mark Lepper <lepperj@Marks-MacBook-Pro-2018.local>
2019-02-08 06:37:05 -0500

    basic files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Splash from "./pages/splash";
 import NotFound from "./pages/notFound";
+import Login from "./pages/login";
 
 import { Router } from "@reach/router";
 
@@ -8,6 +9,7 @@ const App = function() {
   return (
     <Router>
       <Splash path="/" />
+      <Login path="/login" />
       <NotFound default />
     </Router>
   );
---
+++ src/pages/login.js
@@ -0,0 +1,133 @@
+import React, { useEffect } from "react";
+
+const Login = props => {
+  return (
+    <main>
+      <section data-qa="log-in">
+        <div className="carousel no-padding">
+          <div className="carousel-cell">
+            <div className="text">
+              <h1>Welcome</h1>
+              <p>Please log in to continue.</p>
+            </div>
+            <div className="form-container">
+              <form>
+                <fieldset>
+                  <legend>Email Address</legend>
+                  <label htmFor="email-address">
+                    Email Address&#42;
+                    <input
+                      type="email"
+                      name="email-address"
+                      className="email-address"
+                      placeholder="Enter your email address"
+                      autoComplete
+                    />
+                    <span className="is-valid" />
+                  </label>
+                  <span className="error-message">
+                    This is where the error message goes
+                  </span>
+                </fieldset>
+                <fieldset>
+                  <legend>Password</legend>
+                  <label for="password">
+                    Password&#42;
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Enter your password"
+                    />
+                  </label>
+                  <span
+                    toggle="#password"
+                    className="la la-eye-slash show-hide-password"
+                  />
+                  <span className="error-message">
+                    This is where the error message goes
+                  </span>
+                </fieldset>
+                <fieldset>
+                  <legend>Remember Me</legend>
+                  <label for="remember" className="checkbox">
+                    Remember Me
+                    <input type="checkbox" name="remember" id="remember" />
+                  </label>
+                  <span className="forgot-password-prompt">
+                    <a className="button--next">Forgot Password?</a>
+                  </span>
+                  <span className="error-message">
+                    This is where the error message goes
+                  </span>
+                </fieldset>
+                <div className="button-box left">
+                  <a className="button" href="../home/index.html">
+                    <span className="button-label">Log In</span>
+                  </a>
+                </div>
+              </form>
+              <p>
+                <small>
+                  Don't have an account?{" "}
+                  <a href="../register/index.html">Create one now</a>.
+                </small>
+              </p>
+            </div>
+          </div>
+          {/* <div className="carousel-cell">
+            <div className="text">
+              <h1>Reset Your Password</h1>
+              <p>
+                Enter your email address and we'll send you a link to reset your
+                password.
+              </p>
+            </div>
+            <div className="form-container">
+              <form>
+                <fieldset>
+                  <legend>Email Address</legend>
+                  <label for="email-address">
+                    Email Address&#42;
+                    <input
+                      type="email"
+                      name="email-address"
+                      className="email-address"
+                      placeholder="Enter your email address"
+                      autocomplete
+                    />
+                    >
+                  </label>
+                  <span className="error-message">
+                    This is where the error message goes
+                  </span>
+                </fieldset>
+                <div className="button-box left">
+                  <a className="button button--next">
+                    <span className="button-label">Reset Password</span>
+                  </a>
+                </div>
+              </form>
+              <p>
+                <small>
+                  Return to <a className="button--previous">Log In</a>.
+                </small>
+              </p>
+            </div>
+          </div>
+          <div className="carousel-cell">
+            <div className="text">
+              <h1>Password Reset Email Sent</h1>
+              <p>
+                Please check your email inbox. We've sent you a special link
+                that you can use to reset your password.
+              </p>
+            </div>
+          </div>*/}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Login;
